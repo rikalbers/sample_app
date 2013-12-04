9a7acb1de9a3304bde087953534b3eb967d9adea
Rik Albers <rik.albers@student.uva.nl>
2013-12-04 14:42:48 +0100

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user,  only: [:index, :edit, :update]
-  before_action :correct_user,    only: [:edit, :update]
-  before_action :admin_user,      only: :destroy
+  before_action :signed_in_user,       only: [:index, :edit, :update]
+  before_action :correct_user,         only: [:edit, :update]
+  before_action :admin_user,           only: :destroy
+  before_action :not_signed_in_user,   only: [:create, :new]
 
   def index
     @users = User.paginate(page: params[:page])
@@ -9,7 +10,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-  end
+    @microposts = @user.microposts.paginate(page: params[:page])
+  end    
 
   def new
   	@user = User.new
@@ -27,9 +29,14 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "User deleted."
-    redirect_to users_url
+    @user = User.find(params[:id])
+    if current_user?(@user)
+      redirect_to users_path, notice: "You can't destroy yourself."
+    else
+      @user.destroy
+      flash[:success] = "User destroyed."
+      redirect_to users_path
+    end
   end
 
   def edit
@@ -53,13 +60,6 @@ class UsersController < ApplicationController
 
     # Before filters
 
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
-
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
@@ -68,4 +68,9 @@ class UsersController < ApplicationController
     def admin_user
       redirect_to(root_url) unless current_user.admin?
     end
+
+    def not_signed_in_user
+      redirect_to(root_url) if signed_in?
+    end
+
 end
